32d5f78f51bd3647f90970c369441162e73af2ac
Azamat Davletbaev <azamat.davletbaev@gmail.com>
2024-02-25 00:16:19 +0500

    2

---
+++ ParallelCalc/ParallelCalc/Calc.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelCalc
+{
+    internal class Calc
+    {        
+        const int count = 100_000;
+        public int[] items = new int[count];
+
+        public Calc()
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < count; i++)
+                items[i] = rnd.Next(0, 10);
+        }
+
+        /// <summary>
+        /// Делегат, обертка для вызова методов. для удобства замера времени выполнения
+        /// </summary>
+        Func<Func<int>, (int Sum, long Milliseconds)> Watcher = (func) =>
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            int sum = func.Invoke();
+
+            sw.Stop();
+            return (sum, sw.ElapsedMilliseconds);
+        };
+
+        /// <summary>
+        /// обычное суммирование без потоков
+        /// </summary>
+        /// <param name="size">множитель 100k</param>
+        /// <returns></returns>        
+        public (int Sum, long Milliseconds) GetSum(int size) => Watcher(() =>
+        {
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                foreach (var item in items)
+                {
+                    sum += item;
+                }
+            }
+            return sum;
+        });
+
+
+        public (int Sum, long Milliseconds) GetSumByLINQ(int size) => Watcher(() =>
+        {
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += items.AsParallel().Sum();
+            }
+            return sum;
+        });
+
+        private object lockObject = new object();
+
+        public (int Sum, long Milliseconds) GetSumByThread(int size) => Watcher(() =>
+        {
+            int sum = 0;
+
+            Thread[] threads = new Thread[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                threads[i] = new Thread(() =>
+                {
+                    lock (lockObject)
+                    {
+                        sum += items.Sum();
+                    }                    
+                });
+                threads[i].Start();                                
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            return sum;
+        });
+
+
+
+    }
+}
---
+++ ParallelCalc/ParallelCalc/Program.cs
@@ -0,0 +1,38 @@
+﻿using ParallelCalc;
+using System.Drawing;
+
+Console.WriteLine("Домашнее задание по теме: Многопоточный проект");
+
+// делегат, обертка для вызова методов расчета
+Action<Func<int, (int Sum, long Milliseconds)>, int, string> calcAction = (func, count, message) =>
+{
+    var result = func.Invoke(count);
+    Console.WriteLine($"{message} (всего итераций {count*100000}) => {result.Sum} : {result.Milliseconds} мсек");
+};
+
+//количество итераций расчета х100_000
+int k1 = 1;
+int k100 = 100;
+int k1000 = 1000;
+
+var calc = new Calc();
+
+Console.WriteLine($"Инициализирован массив из {calc.items.Count()} элементов");
+
+calcAction(calc.GetSum, k1, "Обычное суммирование");
+calcAction(calc.GetSum, k100, "Обычное суммирование");
+calcAction(calc.GetSum, k1000, "Обычное суммирование");
+
+calcAction(calc.GetSumByLINQ, k1, "Суммирование AsParallel().Sum()");
+calcAction(calc.GetSumByLINQ, k100, "Суммирование AsParallel().Sum()");
+calcAction(calc.GetSumByLINQ, k1000, "Суммирование AsParallel().Sum()");
+
+calcAction(calc.GetSumByThread, k1, "Cуммирование через Threads");
+calcAction(calc.GetSumByThread, k100, "Cуммирование через Threads");
+calcAction(calc.GetSumByThread, k1000, "Cуммирование через Threads");
+
+Console.ReadLine();
+
+
+
+    
